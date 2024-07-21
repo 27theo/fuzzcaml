cdcb3d37bb0194979bd8f3e4ba6a93fd72482df9
Theo Addis <theocaddis@gmail.com>
2024-07-21 20:43:34 +0100

    Upload version 1.0

---
+++ README.md
@@ -0,0 +1,46 @@
+# Fuzzcaml
+
+Asynchronous URL fuzzer built in OCaml with `Lwt` and `Ezcurl`.
+
+## What is a URL fuzzer?
+
+A URL fuzzer is a security tool that automatically discovers hidden or unlinked
+content on web servers by systematically testing URL combinations. Commonly
+used to identify potential vulnerabilities, exposed resources, and map web
+application structures, aiding in security audits and penetration testing.
+
+## Usage
+
+```
+$ dune build
+$ ./fuzzcaml --help
+```
+
+```
+fuzzcaml -t <target_url> [-w <wordlist>] [-s <success_codes>]
+  -t Set target url
+  -w Set wordlist (defaults to wordlists/cve_paths.txt)
+  -s Set success codes (delimited by a comma e.g. -s 200,300,400)
+  -help  Display this list of options
+  --help  Display this list of options
+```
+
+Example:
+
+```
+$ ./fuzzcaml -t https://www.google.com -w wordlists/cve_paths_short.txt -s 200,202,303
+```
+
+```
+[404] Fail https://www.google.com/htmlpurifier/htmlpurifier.includes.php
+[404] Fail https://www.google.com/class.tx_phpunit_testsuite.php
+[404] Fail https://www.google.com/admin/test.php
+[404] Fail https://www.google.com/encapscms-0.3.6/blogs.php
+[404] Fail https://www.google.com/cms/classes/controller.php
+[200] Success https://www.google.com/inc/plugins/changstats.php
+[200] Success https://www.google.com/zip/divers.php
+[303] Success https://www.google.com/frs/admin/qrs.php
+[202] Success https://www.google.com/libraries/dbi/
+[404] Fail https://www.google.com/admin/modules/pages/_locked.php
+[404] Fail https://www.google.com/filemanager/handlers/embed.php
+```
---
+++ bin/main.ml
@@ -0,0 +1,90 @@
+(* Argument specification *)
+let usage_str = "fuzzcaml -t <target_url> [-w <wordlist>] [-s <success_codes>]"
+let target_url = ref ""
+let wordlist_arg = ref ""
+let success_codes_arg = ref ""
+
+let arg_spec =
+  [
+    ("-t", Arg.Set_string target_url, "Set target url");
+    ( "-w",
+      Arg.Set_string wordlist_arg,
+      "Set wordlist (defaults to wordlists/cve_paths.txt)" );
+    ( "-s",
+      Arg.Set_string success_codes_arg,
+      "Set success codes (delimited by a comma e.g. -s 200,300,400)" );
+  ]
+
+(* Utils *)
+let usage_and_exit () =
+  Arg.usage arg_spec usage_str;
+  exit 1
+
+let ensure_trailing_slash url =
+  if String.ends_with ~suffix:"/" url then url else String.cat url "/"
+
+let string_is_empty s = String.equal (String.trim s) ""
+
+(* Where the magic happens... *)
+let fuzz_site urls success_codes =
+  let check_page url =
+    let%lwt result = Ezcurl_lwt.get ~url () in
+    match result with
+    | Ok response ->
+        let msg =
+          if List.mem response.code success_codes then "Success" else "Fail"
+        in
+        Printf.printf "[%d] %s %s\n%!" response.code msg url;
+        Lwt.return_unit
+    | Error (code, err_str) ->
+        let code = Curl.int_of_curlCode code in
+        Printf.printf "Curl error %d : %s\n  %s\n" code url err_str;
+        Lwt.return_unit
+  in
+
+  (* Iterate and wait *)
+  let stream = Lwt_stream.of_list urls in
+  let max_concurrency = 50 in
+  (*                    ^^ Change as required *)
+  let worker () = Lwt_stream.iter_n ~max_concurrency check_page stream in
+  let%lwt () = worker () in
+
+  Lwt.return_unit
+
+let () =
+  Arg.parse arg_spec (fun _ -> usage_and_exit ()) usage_str;
+
+  (* Target URL *)
+  if string_is_empty !target_url then usage_and_exit ();
+  let url = ensure_trailing_slash !target_url in
+
+  (* Success codes *)
+  let success_codes =
+    if string_is_empty !success_codes_arg then [ 200 ]
+    else
+      try List.map int_of_string (String.split_on_char ',' !success_codes_arg)
+      with _ ->
+        prerr_endline
+          "Error: Success codes should be a comma delimited list of integers";
+        exit 1
+  in
+
+  (* Wordlist *)
+  let wordlist =
+    if string_is_empty !wordlist_arg then "wordlists/cve.txt" else !wordlist_arg
+  in
+
+  (* Read pages from wordlist *)
+  let pages =
+    try In_channel.with_open_text wordlist (fun ic -> In_channel.input_lines ic)
+    with _ ->
+      Printf.eprintf "Error: Failed to read lines from wordlist at '%s'\n"
+        wordlist;
+      exit 1
+  in
+
+  (* Concatenate urls ahead of fuzzing *)
+  let urls = List.map (String.cat url) pages in
+
+  (* Perform the fuzzing *)
+  Lwt_main.run (fuzz_site urls success_codes)
